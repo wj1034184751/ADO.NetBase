15bbfb8415ba61aeda8863a5710721b1bd28dc7e
wj <1034184751@qq.com>
2019-04-27 00:21:17 +0800

    测试

---
+++ Code1/ConsoleApp1/ConsoleApp1/DelegateTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    #region
+    public delegate void StringProcessor(string input);
+
+    public class DelegateTest
+    {
+        public static void Run()
+        {
+            Person jon = new Person("Jon");
+            Person tom = new Person("Tom");
+            StringProcessor jonVotice, tomVotice, background;
+            ////创建三个委托实例
+            jonVotice = new StringProcessor(jon.Say);
+            tomVotice = new StringProcessor(tom.Say);
+            background = new StringProcessor(Background.Note);
+            ////调用委托实例
+            jonVotice("Hello,son");
+            tomVotice.Invoke("Hello,Dayy!");
+            background("An airplance files past.");
+        }
+    }
+
+    public class Person
+    {
+        private string name;
+
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public void Say(string message)
+        {
+            Console.WriteLine("{0} says:{1}", name, message);
+        }
+    }
+
+    public class Background
+    {
+        public static void Note(string note)
+        {
+            Console.WriteLine("{0}", note);
+        }
+    }
+    #endregion
+}
--- Code1/ConsoleApp1/ConsoleApp1/Program.cs
+++ Code1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,19 +16,32 @@ namespace ConsoleApp1
             ////自己新建类比较
             //list.Sort(new ProductNameComparer());
             //委托比较一
-           // list.Sort(delegate (Product x, Product y)
-           //{
-           //    return x.Name.CompareTo(y.Name);
-           //});
+            // list.Sort(delegate (Product x, Product y)
+            //{
+            //    return x.Name.CompareTo(y.Name);
+            //});
 
             ////委托比较二
-            list.Sort((x, y) =>
-                x.Name.CompareTo(y.Name));
+            //list.Sort((x, y) =>
+            //    x.Name.CompareTo(y.Name));
 
-            foreach (var item in list.OrderBy(d => d.Name))
-            {
-                Console.WriteLine(item);
-            }
+            //foreach (var item in list.OrderBy(d => d.Name))
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            ////委托三
+            //List<Product> matches = list.FindAll(d => d.Price > 1);
+            //Action<Product> print = Console.WriteLine;
+            //matches.ForEach(print);
+
+            ////委托四
+            //DelegateTest.Run();
+
+            ////强类型
+            object o = "hello world";
+            //Console.WriteLine(o.length);
+            Console.WriteLine(((string)o).Length);
         }
     }
 }
