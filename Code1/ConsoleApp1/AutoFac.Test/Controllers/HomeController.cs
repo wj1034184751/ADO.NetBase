5ace4e57445d7be92fb8dd2127fb9325d52e113e
wj <1034184751@qq.com>
2019-07-02 17:39:21 +0800

    公司提交

---
+++ Code1/ConsoleApp1/AutoFac.Test/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AutoFac.Test.Models;
+using Autofac;
+using AutoFac.Test.AutoFacModel;
+
+namespace AutoFac.Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IDateWriter _dataWrite;
+        private readonly IOutput _output;
+        private readonly ConsoleOutPut _consoleOutPut;
+
+        public HomeController(IDateWriter dataWrite,
+                              IOutput output)
+        {
+            this._dataWrite = dataWrite;
+            this._output = output;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Code1/ConsoleApp1/ConsoleApp1/EnumerableTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class EnumerableTest : IEnumerable<KeyValuePair<string, string>>
+    {
+        private IList<KeyValuePair<string, string>> _params;
+
+        public EnumerableTest()
+        {
+            _params = new List<KeyValuePair<string, string>>();
+        }
+
+        public EnumerableTest(IEnumerable<KeyValuePair<string,string>>parameters)
+        {
+            _params = new List<KeyValuePair<string, string>>(parameters);
+        }
+
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+        {
+            return _params.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _params.GetEnumerator();
+        }
+    }
+}
