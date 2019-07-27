62cc0a3e025a528cfa21e13b452638b23842304c
wj <1034184751@qq.com>
2019-07-27 17:28:35 +0800

    Home

---
+++ Code1/ConsoleApp1/NetMath/Math_Factorial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetMath
+{
+    public class Math_Factorial
+    {
+        public static void Print(int math)
+        {
+            Console.WriteLine($"{math}!={ReturnFactorial(math)}");
+        }
+        static int ReturnFactorial(int math=10)
+        {
+            int result = 1, i;
+            for (i = 2; i <= math; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
