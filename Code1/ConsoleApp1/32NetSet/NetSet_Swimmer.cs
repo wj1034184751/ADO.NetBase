ab0dc11923e8ba5fc8027715dbed12ff40d6b389
1034184751@qq.com <1034184751@qq.com>
2019-12-06 14:41:46 +0800

    Comp

---
+++ Code1/ConsoleApp1/32NetSet/NetSet_Swimmer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _32NetSet
+{
+    public abstract class Event
+    {
+        protected int numLanes;
+        protected ArrayList swimmer;
+
+        public Event(string filename,int lanes)
+        {
+            numLanes = lanes;
+            swimmer = new ArrayList();
+            FileHelper.csFile f = new FileHelper.csFile(filename);
+            f.OpenForRead();
+            string s = f.readLine();
+            while(s!=null)
+            {
+                
+            }
+        }
+
+     
+    }
+
+    public class NetSet_Swimmer
+    {
+    }
+}
